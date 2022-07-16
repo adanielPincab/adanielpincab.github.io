55958075147ba286dacc3d9cbe8b8419b73c6bcc
adanielpincab <adanielpincab@gmail.com>
2022-07-16 17:25:57 +0200

    header hecho

--- scripts/background.js
+++
@@ -1,5 +0,0 @@
-const bg = document.getElementsByClassName('background')[0];
-for ( bubble of bg.children ) {
-    let pos = (-22 + Math.random() *44).toString() + 'vw ' + (-22 + Math.random() *44).toString() + 'vh';
-    bubble.style.transformOrigin = pos;
-}
--- scripts/nav.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementsByTagName('nav')[0];
-
-window.onscroll = function () {  
-    if (window.pageYOffset/window.innerHeight > 0.5){
-        nav.style.opacity = 1;
-    } else {
-        nav.style.opacity = 0;
-    } 
-} 
